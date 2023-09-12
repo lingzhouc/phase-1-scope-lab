bdac71f90a506c6ed6670104551d09100df7ab18
LingZhouChen <lzcciao@gmail.com>
2023-09-12 10:40:18 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+// var global scope
+var customerName = 'bob';
+// const global scope
+const leastFavoriteCustomer = 'smob';
+
+// fn uppercases customerName
+function upperCaseCustomerName() {
+    return customerName = customerName.toUpperCase();
+} 
+
+// fn declare var global scope
+// assign value to undeclared variable => global variable
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+    return bestCustomer;
+} 
+
+// overwrite bestCustomer variable
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+    return bestCustomer;
+}
+
+// try to change const
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'swob';
+    return leastFavoriteCustomer;
+}  
